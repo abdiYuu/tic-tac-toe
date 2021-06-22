eed2b73d8ce21669285ec976d6b519f0f2a84e59
Abdi Yusuf <abdiyuu7@gmail.com>
2021-06-21 20:56:29 -0600

    - win checking function - simple ui for testing - skeleton for display function - skeleton for game logic function

--- app.js
+++ app.js
@@ -7,17 +7,49 @@ const createBoard = function() {
 	const clear = function() {
 		board = new Array(9);
 	}
-	const checkWin = function() {
+	const hasLine = function() {
 		let row = board.some((space, index) => space === board[index-1] && space === board[index+1])
 		let column = board.some((space, index) => space === board[index-3] && space === board[index+3])
 		let diag = board.some((space, index) => space === board[index-4] && space === board[index+4])
-		return row;
+		
+		if([row, column, diag].includes(true)) {return true};
+		return false;
+	}
+	
+	const isFull = function() {
+		return (board.findIndex(position => position === undefined) === -1) ? true : false;
 	}
 
-	return {update, clear, checkWin}
+	return {update, clear, hasLine, isFull, board}
 
 }
 
-gameBoard = createBoard();
+const controlDisplay = function() {
 
+}
+
+const runGame = function() {
+	const start = function(){
+		const grid = document.querySelectorAll('.square')
+		grid.forEach(square => square.addEventListener('click', round));
+	}
+	const restart = {};
+
+	const round = function(e) {
+		let position = Number(e.target.id);
+		gameBoard.update('x', position)
+		e.target.removeEventListener('click', round);
+	}
+
+	const win = {}
+	const tie = {}
+
+	return {start, restart, round, win, tie};
+}
+
+
+
+const gameBoard = createBoard();
+const game = createGame();
+const display = controlDisplay();
 
